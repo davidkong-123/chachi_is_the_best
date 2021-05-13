747ce94238cea1f88783f0eb5e39bf5645b3e2a0
davidkong-123 <abc1265209896@gmail.com>
2021-05-13 03:08:22 -0400

    * in change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_1hc%k^6^-_!aje$!tv$w=4ucj-c11x3^y*j+f^mh!#_=x^2ci'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
